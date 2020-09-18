8621a6e73fb6a93409053f952115a9a95d2f409c
tech3br <dfsdireito95@gmail.com>
2020-09-18 19:32:24 -0300

    adicionando math.js

---
+++ fundamentos/math.js
@@ -0,0 +1,8 @@
+// Math eh um objeto intriseco que contem funcoes e constantes para realizar operacoes matematicas
+const math = Math
+
+const raio = 5.6;
+const area = Math.PI * Math.pow(raio, 2);
+
+console.log(area.toFixed(2))
+console.log(typeof Math)
