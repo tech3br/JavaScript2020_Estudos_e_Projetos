890ef5c0ce964ddfbebdeecda19f763228dc920c
tech3br <dfsdireito95@gmail.com>
2020-09-18 17:43:04 -0300

    update tipagemFraca.js

--- fundamentos/tipagemFraca.js
+++ fundamentos/tipagemFraca.js
@@ -7,8 +7,9 @@ qualquer = 3.1516;
 console.log(qualquer);
 console.log(typeof qualquer);
 
-// evitar nome genericos e siglas - ESCOLHER BONS NOMES É FUNDAMENTAL:
+// evitar nome genericos e siglas como:
 let valor = ""
 let numero = 1
 let pqp = false // Produto Quimico Perigo... kkkk
+//ESCOLHER BONS NOMES É FUNDAMENTAL
 
