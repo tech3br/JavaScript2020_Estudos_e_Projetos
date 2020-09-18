28b2a5004b3f55c21e70c238a231f059294ec48e
tech3br <dfsdireito95@gmail.com>
2020-09-18 17:41:33 -0300

    adicionando estudos no github

---
+++ fundamentos/tipagemFraca.js
@@ -0,0 +1,14 @@
+// texto livre
+let qualquer = 'Legal';
+console.log(qualquer);
+console.log(typeof qualquer);
+
+qualquer = 3.1516;
+console.log(qualquer);
+console.log(typeof qualquer);
+
+// evitar nome genericos e siglas - ESCOLHER BONS NOMES É FUNDAMENTAL:
+let valor = ""
+let numero = 1
+let pqp = false // Produto Quimico Perigo... kkkk
+
