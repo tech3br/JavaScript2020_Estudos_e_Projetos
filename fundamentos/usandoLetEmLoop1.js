e9d412ee87a6981d21a0076ecff210853b6e8717
tech3br <dfsdireito95@gmail.com>
2020-09-19 16:56:16 -0300

    adicionando usandoLetEmLoop1.js e usandoVarEmLoop1.js

---
+++ fundamentos/usandoLetEmLoop1.js
@@ -0,0 +1,5 @@
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+// let i fora do laco de repeticao nao consegue ser impresso pois esta fora do laco
+//console.log('i =', i);
---
+++ fundamentos/usandoVarEmLoop1.js
@@ -0,0 +1,5 @@
+for (var i = 0; i < 10; i++) {
+  console.log(i);
+}
+// var i fora do laco de repeticao eh igual ao valor que terminou a condicao
+console.log('i =', i);
